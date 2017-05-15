2668889756869f639a3c8f1ad393cd773e643493
Adam Cox <mr.adamcox@gmail.com>
2017-05-15 15:02:51 +0200

    move roman map from basemaps to overlays

--- ead/media/js/map/base-layers.js
+++ ead/media/js/map/base-layers.js
@@ -22,22 +22,22 @@ define([
 
     // addition of pelagios roman basemap
     //
-    baseLayers.push({
-        id: 'DARE',
-        name: 'DARE/Pelagios',
-        icon: arches.urls.media + 'img/map/Pelagios.png',
-        layer: new ol.layer.Tile({
-            visible: false,
-            source: new ol.source.XYZ({
-                url: 'http://pelagios.org/tilesets/imperium/{z}/{x}/{y}.png',
-                attributions: [
-                    new ol.Attribution({
-                      html: '<p style="font-size:14px;"><a href="http://pelagios.org/maps/greco-roman/about.html" target="blank">Pelagios project Greco-Roman basemap</a></p>'
-                    })
-                ]
-            })
-        })
-    });
+    // baseLayers.push({
+        // id: 'DARE',
+        // name: 'DARE/Pelagios',
+        // icon: arches.urls.media + 'img/map/Pelagios.png',
+        // layer: new ol.layer.Tile({
+            // visible: false,
+            // source: new ol.source.XYZ({
+                // url: 'http://pelagios.org/tilesets/imperium/{z}/{x}/{y}.png',
+                // attributions: [
+                    // new ol.Attribution({
+                      // html: '<p style="font-size:14px;"><a href="http://pelagios.org/maps/greco-roman/about.html" target="blank">Pelagios project Greco-Roman basemap</a></p>'
+                    // })
+                // ]
+            // })
+        // })
+    // });
 
     return baseLayers;
 });
--- ead/media/js/map/layers.js
+++ ead/media/js/map/layers.js
@@ -7,12 +7,22 @@ define([
     var layers = resourceLayers.layers;
 
     layers.push(new LayerModel({
-        name: layerI18n.exampleLayerName,
-        categories: [layerI18n.exampleCategory],
+        name: 'Greco-Roman Map',
+        categories: ['historical'],
         icon: 'fa fa-bookmark-o',
-        infoContent: layerI18n.exampleContent,
+        infoContent: 'Roman Empire basemap, created by Johan Åhlfeldt with support from the Pelagios project. See credit info in bottom right-hand corner of this page for more information.',
+        // 'Roman Empire basemap, created by Johan Åhlfeldt with support from the Pelagios project. <a href="http://pelagios.org/maps/greco-roman/about.html" target="blank">About this map</a>. <a href="http://dare.ht.lu.se/">interactive version</a>, <a href="http://commons.pelagios.org/2012/09/a-digital-map-of-the-roman-empire/">project background</a></p>',
+        onMap: true,
         layer: new ol.layer.Tile({
-            source: new ol.source.XYZ({url: 'http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png'})
+            visible: false,
+            source: new ol.source.XYZ({
+                url: 'http://pelagios.org/tilesets/imperium/{z}/{x}/{y}.png',
+                attributions: [
+                    new ol.Attribution({
+                      html: '<p style="font-size:14px;"><a href="http://pelagios.org/maps/greco-roman/about.html" target="blank">Pelagios project Greco-Roman basemap</a></p>'
+                    })
+                ]
+            })
         })
     }));
 
