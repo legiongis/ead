1340944cfdac09b7a8e3b66e90b70ac20aa32e19
Adam Cox <mr.adamcox@gmail.com>
2017-05-16 14:02:06 +0200

    add round function to dms conversion (not currently implemented)
    Signed-off-by: Adam Cox <mr.adamcox@gmail.com>

--- ead/media/js/map/map-tools.js
+++ ead/media/js/map/map-tools.js
@@ -1,13 +1,21 @@
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
 // credit: https://stackoverflow.com/questions/5786025/decimal-degrees-to-degrees-minutes-and-seconds-in-javascript
 function ConvertDDToDMS(D, lng){
+    // this could properly round the seconds, but does not "cascade" the rounding up into minutes...
+    //round((0|D*60%1*600)/10,1));
     return {
         dir : D<0?lng?'W':'S':lng?'E':'N',
         deg : 0|(D<0?D=-D:D),
         min : 0|D%1*60,
-        sec : (0|D*60%1*600)/10
+        sec : 0|(D*60%1*600)/10
     };
 }
 
 function DMSString(dmsobj){
     return dmsobj['deg']+'\xB0 '+dmsobj['min']+"' "+dmsobj['sec']+'" '+dmsobj['dir']
 }
+
