2199076add4fd9b7e43f4f523afd91e14fd14443
Adam Cox <mr.adamcox@gmail.com>
2017-05-16 11:53:51 +0200

    modify load_authority_documents() to look for a new column holding Arabic prefLabels

--- ead/setup.py
+++ ead/setup.py
@@ -1,15 +1,20 @@
+import os
 import arches_hip.setup as setup
 from arches.app.models.models import DLanguages
+from management import authority_files_utils
+from django.conf import settings
 
 def install(path_to_source_data_dir=None):
     setup.install()
     add_arabic()
+    load_authority_files(os.path.join(settings.PACKAGE_ROOT, 'source_data', 'concepts', 'authority_files'))
+    
 
 def load_resource_graphs():
     setup.resource_graphs.load_graphs(break_on_error=True)
 
 def load_authority_files(path_to_files=None):
-    setup.authority_files.load_authority_files(path_to_files, break_on_error=True)
+    authority_files_utils.load_authority_files(path_to_files,break_on_error=True)
 
 def load_resources(external_file=None):
     setup.load_resources(external_file)
