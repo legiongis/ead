70171e054d1ec7d76de6627921d2606180e6b6ac
Adam Cox <mr.adamcox@gmail.com>
2017-05-17 09:04:37 +0200

    continue loop don't break it (caused no primary name if a blank name type was encountered before the primary name)

--- ead/models/resource.py
+++ ead/models/resource.py
@@ -140,7 +140,7 @@ class Resource(ArchesResource):
             if lookup:
                 name_type_nodes = name.find_entities_by_type_id(lookup[0])
                 if len(name_type_nodes) == 0:
-                    break
+                    continue
                 if name.find_entities_by_type_id(lookup[0])[0].label == lookup[1]:
                     displayname = name.label
             else:
