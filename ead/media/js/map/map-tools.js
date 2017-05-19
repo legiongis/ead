0f6c43e9b61fcc094b7402ae52a155519be3020e
Billy Williamson <wewill147@gmail.com>
2017-05-19 13:48:12 -0500

    Rounding decimal degrees to dms as expected
    I also have a test file I can include if you want to use it, but delivering the expected value.

--- ead/media/js/map/map-tools.js
+++ ead/media/js/map/map-tools.js
@@ -3,16 +3,50 @@ function round(value, precision) {
     return Math.round(value * multiplier) / multiplier;
 }
 
+// Currently, this creates an instance of a coordinateAsDMS object
+function coordinateAsDMS(dir, d, m, s) {
+    this.dir = dir,
+    this.deg = 0|(d<0?d=-d:d),
+    this.min = 0|m,
+    this.sec = 0|s
+   
+    // If you would rather it return as a generic object, like before
+    // just remove lines 8-11 and uncomment the return statement below
+    // Also, remove the 'new' keyword from the return statement in
+    // ConvertDDToDMS()
+//     return {
+//         dir : dir,
+//         deg : 0|(d<0?d=-d:d),
+//         min : 0|m,
+//         sec : 0|s
+//     };
+}
+
 // credit: https://stackoverflow.com/questions/5786025/decimal-degrees-to-degrees-minutes-and-seconds-in-javascript
 function ConvertDDToDMS(D, lng){
     // this could properly round the seconds, but does not "cascade" the rounding up into minutes...
     //round((0|D*60%1*600)/10,1));
-    return {
-        dir : D<0?lng?'W':'S':lng?'E':'N',
-        deg : 0|(D<0?D=-D:D),
-        min : 0|D%1*60,
-        sec : (0|D*60%1*600)/10
-    };
+    
+    
+    var dir = D<0?lng?'W':'S':lng?'E':'N';
+    var d = Math.floor(Math.abs(D));
+    var minFloat = (Math.abs(D) - d) * 60;
+    var m = Math.floor(minFloat)
+    var s = Math.round((minFloat - m) * 60);
+        
+    // Handles cascading the value at 60
+    if ( s == 60 ) { m++; s = 0 }
+    if ( m == 60 ) { d++; m = 0 }
+
+    // Returns as an instance of coordinateAsDMS()
+    return new coordinateAsDMS(dir, d, m, s);
+    
+//     return {
+//         dir : D<0?lng?'W':'S':lng?'E':'N',
+//         deg : 0|(D<0?D=-D:D),
+//         min : 0|D%1*60,
+//         sec : (0|D*60%1*600)/10
+//     };
 }
 
 function DMSString(dmsobj){
