0899dffa93ceb8cb0ace84e9cd1c5f9e91b800d5
adam cox <mr.adamcox@gmail.com>
2017-10-14 17:35:29 -0500

    deactivate all resources layer by default, re: #12, also rename etc.

--- ead/media/js/map/layers.js
+++ ead/media/js/map/layers.js
@@ -5,6 +5,16 @@ define([
     'layers-i18n'
 ], function(ol, resourceLayers, LayerModel, layerI18n) {
     var layers = resourceLayers.layers;
+    
+    // remove the layer showing all resources from map
+    // also improve the name and description
+    for (i = 0; i < layers.length; i++) {
+        if (layers[i].id == "all") {
+            layers[i].onMap = false;
+            layers[i].name = "All Sites and Complexes";
+            layers[i].description = "";
+        }
+    } 
 
     layers.push(new LayerModel({
         name: 'Greco-Roman Map',
